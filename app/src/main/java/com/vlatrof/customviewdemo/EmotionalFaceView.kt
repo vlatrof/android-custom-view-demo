c53536726d5dfced497df57376511b87630b3d92
vlatrof <gthbttr@gmail.com>
2022-11-14 20:00:01 +0300

    overriding onDraw(canvas: Canvas) method of custom view

--- app/src/main/java/com/vlatrof/customviewdemo/EmotionalFaceView.kt
+++ app/src/main/java/com/vlatrof/customviewdemo/EmotionalFaceView.kt
@@ -4,13 +4,14 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.RectF
 import android.util.AttributeSet
 import android.view.View
 
 class EmotionalFaceView(context: Context, attrs: AttributeSet) : View(context, attrs) {
 
-    // Paint object for coloring
-    private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    // View size in pixels
+    private var size = 320
 
     // Some preinstalled styling values
     private var faceColor = Color.YELLOW
@@ -19,10 +20,44 @@ class EmotionalFaceView(context: Context, attrs: AttributeSet) : View(context, a
     private var borderColor = Color.BLACK
     private var borderWidth = 4.0f
 
-    // View size in pixels
-    private var size = 320
+    // Paint object for coloring
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
 
-    override fun onDraw(canvas: Canvas?) {
+    override fun onDraw(canvas: Canvas) {
+        // call the super method to keep any drawing from the parent side
         super.onDraw(canvas)
+
+        drawFaceBackground(canvas)
+        drawEyes(canvas)
+        drawMouth(canvas)
+    }
+
+    private fun drawFaceBackground(canvas: Canvas) {
+        val radius = size / 2f
+
+        // background fill
+        paint.color = faceColor
+        paint.style = Paint.Style.FILL
+        canvas.drawCircle(size / 2f, size / 2f, radius, paint)
+
+        // border
+        paint.color = borderColor
+        paint.style = Paint.Style.STROKE
+        paint.strokeWidth = borderWidth
+        canvas.drawCircle(size / 2f, size / 2f, radius - borderWidth / 2f, paint)
+    }
+
+    private fun drawEyes(canvas: Canvas) {
+        paint.color = eyesColor
+        paint.style = Paint.Style.FILL
+
+        val leftEyeRect = RectF(size * 0.32f, size * 0.23f, size * 0.43f, size * 0.50f)
+        canvas.drawOval(leftEyeRect, paint)
+
+        val rightEyeRect = RectF(size * 0.57f, size * 0.23f, size * 0.68f, size * 0.50f)
+        canvas.drawOval(rightEyeRect, paint)
+    }
+
+    private fun drawMouth(canvas: Canvas) {
     }
 }
